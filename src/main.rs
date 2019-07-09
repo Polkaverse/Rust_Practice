07758ed8fbedb8fd7f205b85ae61fb4cbac51d11
PankajChaudhary <pankajchaudhary172@gmail.com>
2019-07-09 11:31:41 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//mod print;
+mod varcon;
+
+fn main() {
+    println!("Starting Pankaj chaudhary");
+    //print::run();
+    varcon::con();
+}
+
+
---
+++ src/varcon.rs
@@ -0,0 +1,10 @@
+pub fn con() {
+    let f=5;
+    const PAN: f32 = 5.2;
+
+
+    println!("Our Constant is {} ", PAN);
+    for  x in 0..10 {
+        println!("{}{}", x, f);
+    }
+}
